bd1d19adbc68e7cac2a149552d16c8c4d732bfe8
minyk <minykreva@gmail.com>
2017-11-30 15:07:11 +0900

    Update JRE version to 1.8.0_144. Add example for subgroup.

--- frameworks/etcd/docker/get-bits.sh
+++ frameworks/etcd/docker/get-bits.sh
@@ -7,5 +7,5 @@ cp ../build/distributions/*.zip ./
 cp ../../../sdk/bootstrap/bootstrap.zip ./
 
 echo "Download JRE and libmesos"
-curl -L https://downloads.mesosphere.com/java/jre-8u131-linux-x64-jce-unlimited.tar.gz -o jre-8u131-linux-x64-jce-unlimited.tar.gz
+curl -L https://downloads.mesosphere.com/java/jre-8u144-linux-x64.tar.gz -o jre-8u144-linux-x64.tar.gz
 curl -L https://downloads.mesosphere.io/libmesos-bundle/libmesos-bundle-1.10-1.4-63e0814.tar.gz -o libmesos-bundle-1.10-1.4-63e0814.tar.gz
